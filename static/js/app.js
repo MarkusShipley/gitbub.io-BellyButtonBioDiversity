1cb3847144e6f5fe17086b254ee3d2c64f05af40
MarkusShipley <mrksshipley@gmail.com>
2022-03-28 22:31:09 -0400

    re-written final

--- static/js/app.js
+++ static/js/app.js
@@ -1,146 +1,178 @@
-// Function that populates the dropdown
-//Select #dataset is used because it is referred to as that on line 26 of the html
-function Dropdown(){
-    var select = d3.select("#selDataset");
-    d3.json('samples.json').then((data) => {
-        // console.log(data.names);
 
-        var names = data.names;
-        names.forEach((id) =>{
-            select.append("option")
-            .text(id)
-            .property("value", id)
+//function that updates the dashboards
+
+//function that populates the metadata
+function demoInfo(sample)
+{
+    //use d3.json in order to get the data
+    d3.json("samples.json").then((data) => { 
+        //grab all of the metadata
+        let metaData = data.metadata;
+
+        //filter based on the value of the sample (should return 1 result in an array 
+        //based on the dataset)
+        let result = metaData.filter(sampleResult => sampleResult.id == sample);
+
+        //access index 0 from the array
+        let resultData  = result[0]
+
+        //clear metadata out
+        d3.select("#sample-metadata").html(" "); //clears the HTML out
+
+        //Object.entries to get the value key pairs
+        Object.entries(resultData).forEach(([key, value]) => {
+            d3.select("#sample-metadata")
+                .append("h5").text(`${key}: ${value}`);
+
         });
-
     });
 
-    // initiate Graphs
-    // note: as covered in Phil's study hall on 3/23/22, you can call a fucntion that is built later in the JS
-    demoGraphicInfo('940');
-    graphBuilder('940');
-    
-}
-// OptionChange is the main function that drives everything
-// The OptionChange drives or initiliaizes the updates of the other table, bar chart and Bubble chart
-// By the change in the idnumber from the dataset
-function optionChanged(idNumber){
-    //console.log(idNumber)    
-    
-    // note: as cpvered in Phil's study hall on 3/23/22, you can call a fucntion that is built later in the JS
-     demoGraphicInfo(idNumber);
-     graphBuilder(idNumber);
-
-}
-//creating function of demographic info
-function demoGraphicInfo (idNumber) {
-    // using d3 ro select the metadata div
-    // demoGraphList not declared until line 60
-    // #samples-metadata was used because it is reference in line 32 of the index.html
-    var demoGraphicList = d3.select("#sample-metadata");
-
-    // d3 is importing the metadata frp, tjr samples/json file
-    // => are just shorter or easier ways of declaring functions
-    //Phil covered this is study hall so I took a stab at seeing if I could get it to work
-    //https://www.w3schools.com/js/js_arrow_function.asp
-
-    d3.json('samples.json').then((data) => {
-        // using d3 ro retrieve the the metadata
-        var metadata = data.metadata;
-
-        // filtering metadata by idnumber and acquiring the filtered data
-        //https://www.geeksforgeeks.org/javascript-array-filter-method/
-        var filtered = metadata.filter(d => d.id == idNumber);
-        filtered = filtered[0];
-
-        // making certain the deomGraphicList is cleared out
-        //probably not neded for our homework purposes but in real life, I would want to make sure nothing remained if i updated datasets.
-        demoGraphicList.html("");
-
-        //iteraing through the object and appending the deomGraphicList
-        //https://masteringjs.io/tutorials/fundamentals/filter-key#:~:text=JavaScript%20objects%20don't%20have,()%20function%20as%20shown%20below.
-        //https://stackabuse.com/how-to-filter-an-object-by-key-in-javascript/
-        //https://www.w3schools.com/js/js_objects.asp
-        //This is repopulating the "clear" we made in lines 56
-
-        Object.entries(filtered).forEach(([key,value]) => {
-            demoGraphicList.append("h6").text(`${key} : ${value}`);
-        });
-
-    });
 
 }
 
-// A funtion was created that will build the graphs 
-// First part is declaring variables and setting values so that they can be used when the graphs are generated
-//https://plotly.com/javascript/plotlyjs-function-reference/
-//https://plotly.com/javascript/reference/
-function graphBuilder (idNumber) {
-    
-    d3.json('samples.json').then((data) => {
-        var sampleData = data.samples;
+//function that builds the barchart
+function buildBarChart(sample)
+{
+    let data = d3.json("samples.json");
 
-        // filtering metadata along the idNumber
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-        // lveraging filtering fucntion to get the labels, sample values and ids.
-        var filtered = sampleData.filter(d => d.id == idNumber);
+    d3.json("samples.json").then((data) => { 
+        //grab all of the sample data
+        let sampleData = data.samples;
+
+        //filter based on the value of the sample (should return 1 result in an array 
+        //based on the dataset)
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+
+        //access index 0 from the array
+        let resultData  = result[0]
+        //get otuids. labels and sample values
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+
+        //build bar chart
+        //get the y ticks
+        let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
+        let xValues = sample_values.slice(0, 10);
+        let textLabels = otu_labels.slice(0, 10);
+
+        let barChart = {
+            y: yticks.reverse(),
+            x: xValues.reverse(),
+            text: textLabels.reverse(),
+            type: "bar",
+            orientation: "h"
+        }
+
+        let layout = {
+            title: "Top 10 Belly Button Bacteria"
+        }
+
+        Plotly.newPlot("bar", [barChart], layout);
+
        
-        //Once the filtered var is declared and values set to it, the var can be used with the setting of the other vars
-        var otuIds = filtered[0]['otu_ids'];
-        var sampleValues = filtered[0]['sample_values']
-        var otuLabels = filtered[0]['otu_labels']       
-        // Create the horixontal Bar Chart
-        //data must me siced and then the invesred chosen to get the top ten by belly button
-        //to present in the chart propertly
-        //sampleVlues amd otuIDs and otuLabels are vars declared in line 88-90
-        var barTrace = {
-            x: sampleValues.slice(0,10).reverse(),
-            y: otuIds.map(otu_ids => `OTU ${otu_ids}`).slice(0,10).reverse(),
-            type: 'bar',
-            orientation: "h",
-            text: otuLabels.slice(0,10).reverse(),
-            marker: {
-              color: 'rgb(112, 148, 219)'
-            }
-        };
-
-        var barData = [barTrace];
-        var barLayout = {
-            title: "Top Bacteria per Belly Button",
-            xaxis: {title: "Sample Value"},
-            yaxis: {title: "OTU ID's"}
-        };
-        //used the barData and barLayout vars in the graphBuilder function to pass to the Plotly function to pass paramaters for the graph
-        Plotly.newPlot('bar', barData, barLayout);
-        
-        // Create the Bubble Chart
-        //used the barData and barLayout vars in the graphBuilder function to pass to the Plotly function to pass paramaters for the bubble trace
-        var bubbletrace = {
-            x: otuIds,
-            y: sampleValues,
-            mode: 'markers',
-            text: otuLabels,
-            marker: {
-              size: sampleValues,
-              color: otuIds,
-              colorscale: "Earth"
-            }
-          };
-
-        var bubbledata = [bubbletrace];
-        var bubblelayout = {
-            title: "Bacteria Counts per Belly Button",
-            xaxis: {title: "OTU ID"},
-            yaxis: {title: "Sample Values"}
-        };
-        //Used the bubbletrace, bubbledata and bubble layout vars to pass the values to Plotly to create the newplots
-        Plotly.newPlot('bubble', bubbledata, bubblelayout)
-        //The appraoch with the graphbuilder function was used in anticitpation that it could be re-used and it makes the organization of the required plotly paramters more organizwd
-
     });
+
+
+
+}
+//function that initialized the dashboard
+function initialize()
+{
+    //load the data from the /json file
+    
+     //access the dropdown selector from the indx.html file
+    var select = d3.select("#selDataset");
+
+    //use d3.json in order to get the data
+    d3.json("samples.json").then((data) => { 
+        let sampleNames = data.names; //made an array of just the names
+        console.log(sampleNames);
+
+        //use a foreach in order to create options for each sample in 
+        //the selector
+        sampleNames.forEach((sample) => {
+            select.append("option")
+                .text(sample)
+                .property("value",sample);
+        })
+
+            //when initialized we pass in the information for the first sample we find
+        let sample1 = sampleNames[0];
+
+        //call the function to build the metadata
+        demoInfo(sample1);
+        //call functiion to build the bar chart
+        buildBarChart(sample1);
+        //call function to build the bubble chart
+        buildBubbleChart(sample1);
+       
+    });
+
+
 
 }
 
-Dropdown();
+//function that builds the bubble chart
+function buildBubbleChart(sample)
+{ let data = d3.json("samples.json");
 
-//Originially, I attempted to build the the bonus gauge but just cpoiuld not make it happen.
-//Thus, Bonus skipped for this.
+d3.json("samples.json").then((data) => { 
+    //grab all of the sample data
+    let sampleData = data.samples;
+
+    //filter based on the value of the sample (should return 1 result in an array 
+    //based on the dataset)
+    let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+
+    //access index 0 from the array
+    let resultData  = result[0]
+    //get otuids. labels and sample values
+    let otu_ids = resultData.otu_ids;
+    let otu_labels = resultData.otu_labels;
+    let sample_values = resultData.sample_values;
+
+    //build bubble chart
+
+
+    let bubbleChart = {
+        y: sample_values,
+        x: otu_ids,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+            size: sample_values,
+            color: otu_ids,
+            colocrscale: "Earth"
+        }
+    }
+
+    let layout = {
+        title: "Bacteria Cultures Per Sample",
+        hovermode: "closest",
+        xaxis: {title: "OTU ID"}
+    };
+
+    Plotly.newPlot("bubble", [bubbleChart], layout);
+
+   
+});
+
+
+
+}
+
+
+//function that updates the dashboards
+function optionChanged(item)
+{
+    //call the update to the metadata
+    demoInfo(item);
+     //call the function that builds the bar chart
+    buildBarChart(item);
+    //call function to build the bubble chart
+    buildBubbleChart(item);
+}
+
+//call the initialize function
+initialize();
